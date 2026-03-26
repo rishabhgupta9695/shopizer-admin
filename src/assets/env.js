224de1e1f6c64980702b7d8dd87ace4035cbf05c
Rishabh Gupta <rishabhgupta@Rishabhs-MacBook-Pro.local>
2026-03-26 16:07:41 +0530

    fix: correct API URL in env.js (remove apiUrl prefix, fix quotes)

--- src/assets/env.js
+++ src/assets/env.js
@@ -3,8 +3,8 @@
   
     // Environment variables
 
-    window["env"]["APP_BASE_URL"] = "apiUrl: http://localhost:8080/api",
-    window["env"]["APP_SHIPPING_URL"] = 'http://localhost:9090/api"';
+    window["env"]["APP_BASE_URL"] = "http://localhost:30080/api",
+    window["env"]["APP_SHIPPING_URL"] = 'http://localhost:9090/api';
     window["env"]["APP_MAP_API_KEY"] = '';
     window["env"]["APP_DEFAULT_LANGUAGE"] = 'en';
   })(this);
